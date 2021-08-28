0758db87eb859283980cb413a9b537705225625a
Shawn Currie <shawn.currie0@gmail.com>
2021-08-27 18:51:53 -0700

    update linkedin link

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -10,7 +10,7 @@ function Footer() {
                     </a>
                 </li>
                 <li>
-                    <a href="https://www.linkedin.com/in/shawndcurrie" target="_blank" rel="noopener noreferrer">
+                    <a href="https://www.linkedin.com/in/shcurrie" target="_blank" rel="noopener noreferrer">
                         Linkedin
                     </a>
                 </li>
